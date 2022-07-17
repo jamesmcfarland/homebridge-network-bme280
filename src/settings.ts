dfb150455b9d6f870d0366d5a60297530adc9fac
= <=>
2022-07-17 14:05:44 +0000

    Fixed naming and published

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'NetworkedBME280';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-network-bme280';
+export const PLUGIN_NAME = 'homebridge-networked-bme280';
